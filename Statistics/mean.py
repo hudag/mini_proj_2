1b5f66696278d71c35e310af5beb48c1668aa1f0
Asim <anwarasim77@gmail.com>
2019-11-16 06:43:01 -0500

    Fix: exception in mean

--- Statistics/mean.py
+++ Statistics/mean.py
@@ -2,12 +2,15 @@ from Calculator.addition import addition
 from Calculator.division import division
 
 def mean(data):
-    num_values = len(data)
-    total = 0
-    for num in data:
-        total = addition(total, num)
-    return division(num_values,total)
+    try:
+        num_values = len(data)
+        total = 0
+        for num in data:
+            total = addition(total, num)
+
+        m= division(num_values,total)
     except ZeroDivisionError:
         print("Error: Number Not Valid")
     except ValueError:
         print("Error: Only Numeric Values")
+        return m
