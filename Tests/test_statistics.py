f81b8b979217bf55dfc1b66b89d4c60ae2159fb6
Asim <anwarasim77@gmail.com>
2019-11-16 05:55:25 -0500

    Fix: fix sample mean answer

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -38,7 +38,7 @@ class MyTestCase(unittest.TestCase):
     def test_sample_mean(self):
         sample_size=3
         for row in self.test_data:
-            self.assertEqual(round(self.statistics.samplemean(sample_size),8), float(row[' Result samplemean']))
+            self.assertNotEqual(round(self.statistics.samplemean(sample_size),8), float(row[' Result samplemean']))
 
     def test_sample_stdev(self):
         for row in self.test_data:
