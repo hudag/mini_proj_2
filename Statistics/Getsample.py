c2b4c13113f3b456cfeb7f09c3cb8cb310493204
Asim <anwarasim77@gmail.com>
2019-11-16 04:52:34 -0500

    Fix: typo in sample

--- Statistics/Getsample.py
+++ Statistics/Getsample.py
@@ -2,5 +2,5 @@ from random import random
 
 
 def getSample(data, sample_size):
-    random_values = random.sample(data, k=sample_size)
+    random_values = random.choice(data, k=sample_size)
     return random_values
