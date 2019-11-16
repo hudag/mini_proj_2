89a962cb4ace40b8173d9182729e4045a6d6ff98
hudag <hudagohar16@gmail.com>
2019-11-16 04:09:52 -0500

    fix: function to have no repeats

--- Statistics/samplestdev.py
+++ Statistics/samplestdev.py
@@ -1,16 +1,7 @@
 from Calculator.square_root import square_root
-from Statistics.samplemean import samplemean
-from Calculator.addition import addition
-from Calculator.subtraction import subtraction
-from Calculator.square import square
-from Calculator.division import division
+from Statistics.samplevar import samplevar
 
 def samplestdev(data):
-    total=0
-    x = samplemean(data)
-    n = len(data)
-    for i in data
-        total = addition(total, square(subtraction(x, i)))
-    den = division(n-1,total)
-    samplestd = square_root(den)
+    x = samplevar(data)
+    samplestd = square_root(x)
     return samplestd
