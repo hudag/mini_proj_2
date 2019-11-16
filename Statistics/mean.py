420bd9fcf7c35968b854383b79070b0c900d3e20
Asim <anwarasim77@gmail.com>
2019-11-16 07:30:12 -0500

    Fix: file reader tweaked

--- Statistics/mean.py
+++ Statistics/mean.py
@@ -2,8 +2,9 @@ from Calculator.addition import addition
 from Calculator.division import division
 
 def mean(data):
-    num_values = len(data)
+    num_values = 0
     total = 0
     for num in data:
         total = addition(total, num)
+        num_values = addition(num_values,1)
     return division(total, num_values)
