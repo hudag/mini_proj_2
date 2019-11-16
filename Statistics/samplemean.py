0be35bccb4a5e1b0f953988a5d082023673ff23f
hudag <hudagohar16@gmail.com>
2019-11-16 03:49:12 -0500

    Feature: Standard mean formula

---
+++ Statistics/samplemean.py
@@ -0,0 +1,12 @@
+from Calculator.Addition import addition
+from Calculator.Division import division
+from Statistics.Getsample import getSample
+
+
+def sample_mean(data, sample_size):
+    total = 0
+    sample = getSample(data, sample_size)
+    num_values = len(sample)
+    for num in sample:
+        total = addition(total, num)
+    return division(total, num_values)
