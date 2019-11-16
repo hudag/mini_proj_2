eb32d1e33fc68618c84eb471d1e6ee20cb29e71c
Asim <anwarasim77@gmail.com>
2019-11-16 08:34:40 -0500

    Task: file not found exception

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -10,11 +10,13 @@ class CsvReader:
 
     def __init__(self, filepath):
         self.data = []
-
-        with open(absolutepath(filepath)) as text_data:
-            csv_data = csv.DictReader(text_data, delimiter=',')
-            for row in csv_data:
-                self.data.append(row)
+        try:
+            with open(absolutepath(filepath)) as text_data:
+                csv_data = csv.DictReader(text_data, delimiter=',')
+                for row in csv_data:
+                    self.data.append(row)
+        except FileNotFoundError:
+            print("Invalid file path")
         pass
 
     def return_data_as_objects(self, class_name):
