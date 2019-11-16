114e7f34c80b04a45aec10d3a8f0b561c324d489
hudag <hudagohar16@gmail.com>
2019-11-16 01:12:03 -0500

    Fix: standard deviation formula so no repeats

--- Statistics/stdev.py
+++ Statistics/stdev.py
@@ -1,19 +1,7 @@
-import math
-from Statistics.mean import mean
-from Calculator.addition import addition
-from Calculator.subtraction import subtraction
-from Calculator.square import square
-from Calculator.division import division
+from Statistics.popvar import popvar
 from Calculator.square_root import square_root
 
-
 def stdev(data):
-
-    total = 0
-    x = mean(data)
-    n = len(data)
-    for i in data:
-        total = addition(square(subtraction(x,i)))
-    new = division(total,n)
-    std = square_root(new2)
+    base= popvar(data)
+    std = square_root(base)
     return std
