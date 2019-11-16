b73ce249e48316bbf84c88618c0b3a58d429e5c5
hudag <hudagohar16@gmail.com>
2019-11-15 23:42:14 -0500

    feature: test to check mean

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,1 +1,14 @@
+from Calculator.Calculator import Calculator
+from Statistics.mean import mean
+from CsvReader.CsvReader import CsvReader
 
+class Statistics(Calculator):
+    data = []
+
+    def __init__(self, filepath):
+        self.data = CsvReader(filepath)
+        super().__init__()
+
+    def mean(self):
+        self.result = mean(self.data)
+        return self.result
