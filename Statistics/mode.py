56b202ed161e4efec5cb088d9dfad4ad84e67180
Asim <anwarasim77@gmail.com>
2019-11-16 08:24:47 -0500

    Fix: fixed typo in mode

--- Statistics/mode.py
+++ Statistics/mode.py
@@ -3,7 +3,7 @@ from collections import Counter
 def mode(arr):
     n = len(arr)
 
-    data = Counter(n_num)
+    data = Counter(arr)
     get_mode = dict(data)
     mode = [k for k, v in get_mode.items() if v == max(list(data.values()))]
     return mode
